03a85b4eb52d9fc43b53da80628490f38ee99b75
Marcel Maltry <mmaltry+github@mailbox.org>
2021-09-15 14:49:55 +0200

    Add reference implementation in rmi_build.
    List of changes:
    * Conduct build time experiment for reference implementation.
    * Measure and plot all possible configurations.
    * Decrease experiment timeout to 60sec.

--- scripts/run_rmi_build.sh
+++ scripts/run_rmi_build.sh
@@ -13,7 +13,7 @@ BIN="build/bin/rmi_build"
 # Set number of repetitions and samples
 N_REPS="3"
 PARAMS="--n_reps ${N_REPS}"
-TIMEOUT=$((${N_REPS}*3000))
+TIMEOUT="60s"
 
 DATASETS="books_200M_uint64 fb_200M_uint64 osm_cellids_200M_uint64 wiki_ts_200M_uint64"
 LAYER1="cubic_spline linear_spline linear_regression radix"
@@ -44,48 +44,81 @@ then
 fi
 
 # Write csv header
-echo "dataset,n_keys,layer1,layer2,n_models,bounds,size_in_bytes,rep,build_time,checksum" > ${FILE_RESULTS} # Write csv header
+echo "dataset,n_keys,rmi,layer1,layer2,n_models,bounds,size_in_bytes,rep,build_time,checksum" > ${FILE_RESULTS} # Write csv header
 
-# Run layer1 model type experiment
+# Run layer1 and layer 2 model type experiment
 for dataset in ${DATASETS};
 do
-    echo "Performing ${EXPERIMENT}: layer1 on '${dataset}'..."
+    echo "Performing ${EXPERIMENT} (ours) on '${dataset}'..."
     for ((i=6; i<=25; i += 1));
     do
         n_models=$((2**$i))
         for l1 in ${LAYER1};
         do
-            run ${dataset} ${l1} linear_spline ${n_models} none
+            for l2 in ${LAYER2};
+            do
+                for bound in ${BOUNDS};
+                do
+                    run ${dataset} ${l1} ${l2} ${n_models} ${bound}
+                done
+            done
         done
     done
 done
 
-# Run layer2 model type experiment
+
+# Prepare reference implementation experiment
+CWD=$(pwd)
+RMI_PATH="third_party/RMI"
+TMP_PATH="${CWD}/${RMI_PATH}/tmp"
+MANIFEST_FILE="${CWD}/${RMI_PATH}/Cargo.toml"
+NAMESPACE="tmp"
+RESULTS_FILE=${CWD}/${FILE_RESULTS}
+mkdir -p ${TMP_PATH}
+cd ${TMP_PATH}
+
+declare -A l1models
+l1models['linear_spline']="linear_spline"
+l1models['cubic_spline']="cubic"
+l1models['linear_regression']="linear"
+l1models['radix']="radix"
+
+declare -A l2models
+l2models['linear_spline']="linear_spline"
+l2models['linear_regression']="linear"
+
+declare -A bounds
+bounds['labs']=""
+bounds['none']="--no-errors"
+
+# Run reference implementation experiment
 for dataset in ${DATASETS};
 do
-    echo "Performing ${EXPERIMENT}: layer2 on '${dataset}'..."
+    DATA_FILE="${CWD}/${DIR_DATA}/${dataset}"
+    echo "Performing ${EXPERIMENT} (ref) on '${dataset}'..."
     for ((i=6; i<=25; i += 1));
     do
         n_models=$((2**$i))
-        for l2 in ${LAYER2};
+        for l1 in ${!l1models[@]};
         do
-            run ${dataset} cubic_spline ${l2} ${n_models} none
-            run ${dataset} linear_spline ${l2} ${n_models} none
+            for l2 in ${!l2models[@]};
+            do
+                for bound in ${!bounds[@]};
+                do
+                    for ((rep=0; rep<${N_REPS}; rep += 1));
+                    do
+                        # Build RMI.
+                        cargo run --manifest-path ${MANIFEST_FILE} --release -- ${DATA_FILE} ${NAMESPACE} ${l1models[${l1}]},${l2models[${l2}]} ${n_models} ${bounds[${bound}]} > /dev/null
+
+                        # Exract results.
+                        size=$(cat ${TMP_PATH}/tmp.h | grep SIZE | sed 's/.*=//' | tr -d -c 0-9)
+                        build_time=$(cat ${TMP_PATH}/tmp.h | grep BUILD | sed 's/.*=//' | tr -d -c 0-9)
+
+                        # Append results to csv.
+                        echo "${dataset},200000000,ref,${l1},${l2},${n_models},${bound},${size},${rep},${build_time},0" >> ${RESULTS_FILE}
+                    done
+                done
+            done
         done
     done
 done
-
-# Run bound type experiment
-for dataset in ${DATASETS};
-do
-    echo "Performing ${EXPERIMENT}: bounds on '${dataset}'..."
-    for ((i=6; i<=25; i += 1));
-    do
-        n_models=$((2**$i))
-        for bound in ${BOUNDS};
-        do
-            run ${dataset} linear_spline linear_regression ${n_models} ${bound}
-        done
-    done
-done
-
